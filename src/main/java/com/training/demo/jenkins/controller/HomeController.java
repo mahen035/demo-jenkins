b7c9621946a789ad79780263d2b6f50624123836
mkumar <test@mail.com>
2022-06-15 18:57:37 +0530

    added controller

---
+++ src/main/java/com/training/demo/jenkins/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.training.demo.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+	
+	@GetMapping("/msg")
+	public String getMsg() {
+		return "Jenkins process done successfully!!";
+	}
+
+}
